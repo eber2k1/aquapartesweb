2109f8f0e047d9863b776f228501bc2f7e6f1c97
eber2k1 <lit27k@gmail.com>
2025-06-09 10:42:06 -0500

    Actualizando CartonIcon.jsx

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -73,7 +73,7 @@ const CartIcon = () => {
           onClick={toggleCart}
           aria-label="Carrito"
         >
-          <div className="relative">
+          <div className="flex items-center">
             <svg 
               xmlns="http://www.w3.org/2000/svg" 
               className="h-6 w-6 text-white lg:text-gray-700 hover:text-blue-600 transition-colors" 
